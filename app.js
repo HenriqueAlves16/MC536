48bbe5399e4e90fb3ab97a3316d4b60012970146
Henrique Alves de Fernando <h236538@dac.unicamp.br>
2024-11-16 16:16:21 -0300

    inserting to db

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ function mostrarFormulario() {
     // Mostra o formulário selecionado
     const entidade = document.getElementById('entidade').value;
     if (entidade) {
-        document.getElementById(`form-${entidade}`).style.display = 'block';
+        document.getElementById(`form-${entidade}-container`).style.display = 'block';
     }
+}
+
+function showToast(message) {
+    const toast = document.getElementById('toast');
+    toast.textContent = message; // Define a mensagem
+    toast.classList.add('show'); // Adiciona a classe que mostra o toast
+
+    setTimeout(() => {
+        toast.classList.remove('show');
+    }, 3000);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -17,7 +27,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const entidade = document.getElementById('entidade').value;
         if (entidade) {
-            document.getElementById(`form-${entidade}`).style.display = 'block';
+            document.getElementById(`form-${entidade}-container`).style.display = 'block';
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('form-doenca').addEventListener('submit', function (e) {
+        e.preventDefault(); // Impede o envio padrão do formulário
+
+        // Cria um objeto JSON a partir dos dados do formulário
+        const jsonData = {
+            nome_doenca: document.getElementById('nome_doenca').value,
+            letalidade: document.getElementById('letalidade').value,
+            contagioso: document.getElementById('contagioso').value,
+        };
+
+        // Envia os dados para o servidor
+        fetch('http://localhost:3000/inserir_doenca', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', // Define o formato da requisição como JSON
+            },
+            body: JSON.stringify(jsonData), // Envia os dados como JSON
+        })
+        .then(response => response.text()) // Espera uma resposta do servidor
+        .then(data => {
+            showToast('Dados inseridos com sucesso!');
+        })
+        .catch(error => {
+            showToast('Erro ao inserir dados!');
+        });
+    });
+});
+
+
